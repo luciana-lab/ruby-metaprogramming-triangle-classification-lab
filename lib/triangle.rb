c92db59f7ea371c6267a2836b6baa0759fff26ac
Luciana Sugeta <lulusugeta@gmail.com>
2021-03-25 15:42:11 -0400

    commit tree 51a340cc803644effb8067c2395abef8bcdac842

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
+#couldn't resolve this one. Use this https://github.com/learn-co-students/triangle-classification-v-000/pull/43/files
+require 'pry'
 class Triangle
-  # write code here
+  attr_accessor :hypotenuse, :adjacent, :opposite, :all_triangles
+
+  def initialize(hypotenuse, adjacent, opposite)
+    @hypotenuse = hypotenuse
+    @adjacent = adjacent
+    @opposite = opposite
+    @all_triangles = [hypotenuse, adjacent, opposite].sort
+  end
+
+  def kind
+    #binding.pry
+    if triangle_illegal
+      raise TriangleError
+    elsif all_triangles.uniq.length == 1
+      :equilateral
+    elsif all_triangles.uniq.length == 2
+      :isosceles
+    else 
+      :scalene
+    end
+  end
+
+  def triangle_illegal
+    #binding.pry
+    all_triangles.any? {|side| side <= 0} || all_triangles[0] + all_triangles[1] <= all_triangles[2]
+  end
+
+  class TriangleError < StandardError
+
+  end
+
 end
